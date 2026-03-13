709b43aec71a35f9529c7435416f92fc56abc390
Stellan Cook <stellancook@gmail.com>
2026-03-12 21:46:50 -0700

    attack appears but does not disappear

--- attack.gd
+++ attack.gd
@@ -4,10 +4,11 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().create_timer(0.3).timeout.connect(queue_free)
+	print("creating instance")
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
-		area.damage
+		area.take_damage(10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
