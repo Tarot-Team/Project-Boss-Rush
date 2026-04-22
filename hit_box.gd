9383604f9f3a7f7ca73e88472a5de1757c7cc516
TeotlRomulo <tromulo@pipeline.sbcc.edu>
2026-04-21 23:58:01 -0700

    added health change item

--- hit_box.gd
+++ hit_box.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var interaction_area = $InteractionArea
-
+@onready var player = get_tree().get_nodes_in_group("player")[0]
 func _ready():
+	
 	interaction_area.interact = Callable(self, "_on_interact")
 
 func _on_interact():
-	print("yes")
+		player.take_damage(-1)
+	
